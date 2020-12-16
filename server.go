984853b53d604c2412318c2ba44e521c1946120f
Umang Parmar <umangjparmar@gmail.com>
2020-12-16 19:52:17 +0530

    add closures returning skeleton handlers

--- server.go
+++ server.go
@@ -1,7 +1,22 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+)
 
 type env struct {
 	db *sql.DB
+}
+
+func shorten(e *env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func elongate(e *env) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 }
