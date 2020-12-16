2dc098454ca918117f379e89156537a023f1d1bd
Umang Parmar <umangjparmar@gmail.com>
2020-12-16 20:02:50 +0530

    create servemux and initialise routes

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"net/http"
+	"os"
 )
 
 type env struct {
@@ -24,4 +25,15 @@ func elongate(e *env) http.HandlerFunc {
 func routes(m *http.ServeMux, e *env) {
 	m.Handle("/shorten", shorten(e))
 	m.Handle("/elongate", elongate(e))
+}
+
+func main() {
+	db, err := connect(os.Getenv("DB_USER"), os.Getenv("DB_PASSWD"), os.Getenv("DB_URL"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	if err != nil {
+		panic(err)
+	}
+	e := env{db: db}
+	mux := http.NewServeMux()
+	routes(mux, &e)
+	http.ListenAndServe(":9090", mux)
 }
