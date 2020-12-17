bac02995fb844711ca561e69c2529e8dcbb3b269
Umang Parmar <umangjparmar@gmail.com>
2020-12-17 15:41:47 +0530

    store hash in url struct after calculating

--- url.go
+++ url.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"time"
 )
@@ -11,12 +12,12 @@ type url struct {
 	original string
 }
 
-func (u *url) calculateHash() string {
+func (u *url) calculateHash() {
 	h := sha1.New()
 	h.Write([]byte(u.original))
 	h.Write([]byte(time.Now().UTC().Format(time.RFC3339Nano)))
-	bs := h.Sum(nil)
-	return string(bs)[:8]
+	bs := hex.EncodeToString(h.Sum(nil))
+	u.hash = string(bs)[:8]
 }
 
 func (u *url) toJSON() []byte {
